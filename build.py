b9d8295aabe8e3a5ac9091b9adf47f1a41d8e0a6
utk1801 <utk1801@users.noreply.github.com>
2017-06-13 19:35:46 +0000

    Done

--- build.py
+++ build.py
@@ -2,6 +2,5 @@ import numpy as np
 
 
 def solution(l):
-    """
-    Enter your code here
-    """
+    return np.array(l)
+#print solution([12.23, 13.32, 100, 36.32]),type([12.23, 13.32, 100, 36.32])
